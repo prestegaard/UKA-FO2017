4454c7a9c397a2ba918dd36e57c0b4f8595a6343
prestegaard <hakonp@gmail.com>
2017-09-02 17:56:14 +0200

    [UKA-1] Added libraries for FastLEDs and RF24

---
+++ RF24-master/examples/GettingStarted/Debug/GettingStarted.cpp.d
@@ -0,0 +1,18 @@
+C:\Users\Haagon\AppData\Local\Temp\VMBuilds\GettingStarted\nano_atmega328\Debug\GettingStarted.cpp.o: \
+ C:\Users\Haagon\AppData\Local\Temp\VMBuilds\GettingStarted\nano_atmega328\Debug\GettingStarted.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs/../standard/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SPI\src/SPI.h \
+ C:\Users\Haagon\Documents\Arduino\libraries\RF24-master/RF24.h \
+ C:\Users\Haagon\Documents\Arduino\libraries\RF24-master/RF24_config.h \
+ C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2017\Community\Common7\IDE\Extensions\iznu5qi1.uuf\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h
